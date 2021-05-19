e8fe7660a8d15b692f879c8cb3559d36d58ade69
ujw0l <bktujwol@gmail.com>
2021-05-19 13:03:33 +0545

    Removed console log

--- js/mas-gal.js
+++ js/mas-gal.js
@@ -1,6 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    console.log(new jsMasonry());
     new jsMasonry('.mas-gal-gallery', { percentWidth: true, callback: el => el.style.opacity = '' });
 
     const overlay = new jsOverlay();
